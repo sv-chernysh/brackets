007df4d245398c0ddf9a8206d5d92436086f45dc
Serhii Chernysh <srg.chernysh@gmail.com>
2020-03-22 17:59:27 +0200

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
-}
+    if (str.length % 2) return false;
+    let newStr = str;
+    while (newStr.length % 2 === 0 && newStr !== "") {
+        let checkLength = newStr.length;
+        for (let i = 0; i < bracketsConfig.length; i++) {
+            let position = newStr.indexOf(bracketsConfig[i].join(''));
+            if (position !== -1) {
+                newStr = newStr.slice(0, position) + newStr.slice(position + 2);
+            }
+        }
+        if (checkLength === newStr.length) break;
+    }
+    return !newStr;
+};
